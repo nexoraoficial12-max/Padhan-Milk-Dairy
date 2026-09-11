56dfc286459754540fb9bf447e6e9fa57dcf03e9
nexoraoficial12-max <gagansubratpradhan412@gmail.com>
2026-09-11 18:43:29 +0530

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Padhan Milk Dairy',
+  description: 'Fresh Milk, Paneer, Ghee, Dahi and Sweets',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="or">
+      <body>{children}</body>
+    </html>
+  );
+}
